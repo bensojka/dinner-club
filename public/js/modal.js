83d98dd5c611d490b3793fd876653327687c8495
Jay Sullivan <JaySull514@gmail.com>
2017-10-08 01:10:31 -0500

    added z-index in opening and closing the modal don't know if its necessary

--- public/js/modal.js
+++ public/js/modal.js
@@ -16,10 +16,12 @@ function openModal(){
     const modalWrapper = $('.modal-wrapper');
     modalWrapper.css('opacity', '1');
     modalWrapper.css('visibility', 'visible');
+    modalWrapper.css('z-index', '10')
 }
 
 function closeModal(){
     const modalWrapper = $('.modal-wrapper');
     modalWrapper.css('opacity', '0');
     modalWrapper.css('visibility', '0');
+    modalWrapper.css('z-index', '-1')
 }
