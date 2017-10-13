4d2eafe4fd7e79638020f6d574515f2e3a16af7f
Jay Sullivan <JaySull514@gmail.com>
2017-10-12 21:12:42 -0500

    Simplifying tables

--- public/js/group.js
+++ public/js/group.js
@@ -1,8 +1,9 @@
 $(function(){
     const groupContents = $('.group-contents');
 
-    $('.group-btn').on('click', function(){
+    $('.group-btn').click(function(){
         expandGroup();
+
     })
 });
 
@@ -10,8 +11,9 @@ function expandGroup(){
     const group = $('.group');
     const contents = $('.group-contents');
 
-    if(contents.hasClass('.closed')){
-        contents.removeClass('.closed');
-        contents.addClass('.open');
+    console.log(contents.hasClass('closed'));
+
+    if(contents.hasClass('closed')){
+        contents.removeClass('closed').addClass('open');
     }
 }
