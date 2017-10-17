3f0f76a5713f28da4a4b645e3c2f6f4f959586c0
Ben Sojka <bensojka@gmail.com>
2017-10-17 15:49:45 -0500

    removed unused routes

--- routes/hasPermissions.js
+++
@@ -1,9 +0,0 @@
-var hasPermissions = function (req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  req.flash("error", "You need to log in to do that!");
-  res.redirect('/polls');
-}
-
-module.exports = hasPermissions;
