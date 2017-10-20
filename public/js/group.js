c70bcd566ead573b3aa8856ac5cab1d4516eb3a7
Jay Sullivan <JaySull514@gmail.com>
2017-10-20 15:34:59 -0500

    Redid groups page and style

--- public/js/group.js
+++ public/js/group.js
@@ -1,4 +1,49 @@
 $(() => {
+
+  let groupsArr = [[1, 'Jay\'s Group']];
+  let usersArr = [[1, 'Jay@Jay.Jay']];
+  let locationsArr = [[1, 'Applebees']];
+  let referenceArr = [[1, 1, 1]];
+
+  $(document).ready(() => {
+
+    for(let i = 0; i < groupsArr.length; i += 1){
+      const groupList = $('.group-list');
+      let newGroup = new $('<div/>', {class: 'group', id: groupsArr[i][0]});
+      let newGroupBtn = new $('<button/>', {class: 'group-open btn', id: groupsArr[i][1], text: groupsArr[i][1]});
+      newGroup.append(newGroupBtn);
+      for(let j = 0; i < referenceArr.length; i =+ 1){
+        let newUser = $('<p/>', {class: 'group-user', id: referenceArr[i][0], text: usersArr[0][1]});
+        newGroup.append(newUser);
+      }
+        let joinButton = $('<button/>', {class: 'user-join btn', text: "Join"});
+        newGroup.append(joinButton);
+        groupList.append(newGroup);
+    }
+
+    $(document).on('click', '.group-open', function(event){
+      event.preventDefault();
+      const group = $('.active-group');
+      let activeGroup = new $('<div/>', {class: "current"});
+      let activeGroupName = new $('<h1/>', {class: 'active-group-name', text: groupsArr[0][1]});
+      activeGroup.append(activeGroupName);
+      let activeGroupVotes = new $('<p/>', {class: 'active-group-vote', text: locationsArr[0][1] + ' ' + referenceArr[0][2]});
+      activeGroup.append(activeGroupVotes);
+      group.html(activeGroup);
+    });
+
+    for(let i = 0; i < locationsArr.length; i += 1){
+      const locations = $('.locations');
+      let newLocation = new $('<div/>', {class: 'location', id: locationsArr[i][0]});
+      let locationName = new $('<h1/>', {class: 'location-name', text: locationsArr[i][1], id: locationsArr[i][0]});
+      let locationVote = new $('<button/>', {class: 'location-vote-btn btn', text: 'Vote!', id: locationsArr[i][0]});
+      newLocation.append(locationName);
+      newLocation.append(locationVote);
+      locations.append(newLocation);
+    }
+
+  });
+
   $(document).on('click', '.group-btn', function () {
     const contents = $($(this).next('div'));
 
